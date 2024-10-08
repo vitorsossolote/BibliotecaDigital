531ca1c7ee2796e246a03b0e509f8638a7b76180
Vitor Sossolote <vitorchg11@gmail.com>
2024-10-08 11:31:09 -0300

    gitignore

--- app/src/components/trending/books/index.jsx
+++
@@ -1,9 +0,0 @@
-import { View, Pressable, Text, Image    } from 'react-native';
-
-export default function CardHorizontalBook() {
- return (
-    <Pressable>
-        <Text>ITEM</Text>
-    </Pressable>
-  );
-}
