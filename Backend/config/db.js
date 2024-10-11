cc005064cf004e8dd15472909be5e30fa6b6450d
Vitor Sossolote <vitorchg11@gmail.com>
2024-10-11 14:47:38 -0300

    Animações

---
+++ Backend/config/db.js
@@ -0,0 +1,13 @@
+//Incluir biblioteca de conexão
+const mysql = require("mysql2/promise");
+
+//Criar conexão com banco de dados
+const client = mysql.createPool({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "bibliotecainteligente"
+});
+
+module.exports = client;
---
+++ Backend/src/routers.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const clientController = require("../controller/controller");
+const router = express.Router();
+
+router.get("/", clientController.getRoot);
+router.post("/api/createStudent", clientController.createNewStudent); //Cadastrar novo usuario
+router.post("/api/createBiblio", clientController.createNewBiblio); //Cadastrar novo Bibliotecario
+router.post("/api/validadeStudent", clientController.loginStudent);//Validar o login
+router.post("/api/validadeBiblio", clientController.loginBiblio);//Validar o login
+
+// router.post("/api/contato", clientController.createNewMensagem)
+
+// router.post('/api/reset', clientController.getEmailReset)
+// router.post('/api/resetpassword', clientController.ResetSenha)
+
+// router.post('/api/pedidos', clientController.createPedido);
+// router.delete('/api/pedidos/:id', clientController.deletePedido);
+// router.get('/api/pedidos', clientController.getAllPedidos);
+
+module.exports = router;
